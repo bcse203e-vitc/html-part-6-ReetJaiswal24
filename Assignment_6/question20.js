3ec8d0d3bf38c82b68fe09fd34a9fc325df11849
Reet Jaiswal <reet.jaiswal2023@vitstudent.ac.in>
2025-02-04 15:34:01 +0530

    Create question20.js

---
+++ Assignment_6/question20.js
@@ -0,0 +1,13 @@
+function countCharacterOccurrences(str) {
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    return charCount;
+}
+
+// Example usage:
+let inputString = "hello";
+let charOccurrences = countCharacterOccurrences(inputString);
+console.log(`Character occurrences in "${inputString}":`, charOccurrences);
+alert(`Character occurrences in "${inputString}": ${JSON.stringify(charOccurrences)}`);
