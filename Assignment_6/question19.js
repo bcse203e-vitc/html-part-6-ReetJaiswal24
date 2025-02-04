7a8200fc9b0c18ed281910d98040078acc49261b
Reet Jaiswal <reet.jaiswal2023@vitstudent.ac.in>
2025-02-04 15:33:36 +0530

    Create question19.js

---
+++ Assignment_6/question19.js
@@ -0,0 +1,16 @@
+function findLongestWord(sentence) {
+    let words = sentence.split(' ');
+    let longestWord = words[0];
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
+
+// Example usage:
+let inputSentence = "I love programming in JavaScript";
+let longestWord = findLongestWord(inputSentence);
+console.log(`The longest word in the sentence is "${longestWord}"`);
+alert(`The longest word in the sentence is "${longestWord}"`);
