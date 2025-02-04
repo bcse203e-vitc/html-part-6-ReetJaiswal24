d96b1aee920e5faddeabf29ba14a0fe9314a2383
Reet Jaiswal <reet.jaiswal2023@vitstudent.ac.in>
2025-02-04 14:48:07 +0530

    Create question3.js

---
+++ Assignment_6/question3.js
@@ -0,0 +1,13 @@
+function isEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+// Example usage:
+let num = parseInt(prompt("Enter a number:"));
+let result = isEvenOrOdd(num);
+console.log(`The number ${num} is ${result}`);
+alert(`The number ${num} is ${result}`);
