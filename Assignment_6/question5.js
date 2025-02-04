98ed53837639572625c621a600ebb54233f1cd2a
Reet Jaiswal <reet.jaiswal2023@vitstudent.ac.in>
2025-02-04 15:04:23 +0530

    Create question5.js

---
+++ Assignment_6/question5.js
@@ -0,0 +1,14 @@
+function countDigits(number) {
+    let count = 0;
+    while (number !== 0) {
+        number = Math.floor(number / 10);
+        count++;
+    }
+    return count;
+}
+
+// Example usage:
+let num = parseInt(prompt("Enter a number:"));
+let digitCount = countDigits(num);
+console.log(`The number of digits in ${num} is ${digitCount}`);
+alert(`The number of digits in ${num} is ${digitCount}`);
